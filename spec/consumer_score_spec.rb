e70fcbb4f5904e2067ce8454e47ab7bf1effaad6
Kelmer Perez <kelmerp@gmail.com>
2013-11-16 18:31:35 -0600

    fixed spec error

--- spec/consumer_score_spec.rb
+++ spec/consumer_score_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe ConsumerScore do
-  let (:score) {ConsumerScore.new}
+  before (:each) do
+    @score = ConsumerScore.new
+  end
 
   describe "#get_score" do
-    it "takes 3 parameters" do
-      expect(:score).to respond_to(:get_score).with(3).arguments
+    it "takes 3 arguments" do
+      expect(@score).to respond_to(:get_score).with(3).arguments
+    end
+
+    it "is given a valid arguments
     end
   end
 end
