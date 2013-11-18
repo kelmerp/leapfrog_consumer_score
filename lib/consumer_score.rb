50ae9af40b3803803fc5911a4e8ef23659efbb39
Kelmer Perez <kelmerp@gmail.com>
2013-11-18 12:23:43 -0600

    added error checking to arguments

--- lib/consumer_score.rb
+++ lib/consumer_score.rb
@@ -9,9 +9,21 @@ class ConsumerScore
   BASE_URL = "http://internal.leapfrogonline.com"
 
   def get_score(income, zipcode, age)
-    url = "#{BASE_URL}/consumer_scoring?income=#{income}&zipcode=#{zipcode}&age=#{age}"
-    response = HTTParty.get(url)
-    response.body
+    begin
+      if income.class != Fixnum && income.class != Float
+        raise ArgumentError, "An error has occurred: please supply a valid income"
+      elsif !zipcode.to_s =~ /\d{5}/
+        raise ArgumentError, "An error has occurred: please supply a valid zip code"
+      elsif age.class != Fixnum
+        raise ArgumentError, "An error has occurred: please supply a valid age"
+      else      
+        url = "#{BASE_URL}/consumer_scoring?income=#{income}&zipcode=#{zipcode}&age=#{age}"
+        response = HTTParty.get(url)
+        response.body
+      end
+    rescue Exception => e
+      e.message
+    end
   end
   
 end
